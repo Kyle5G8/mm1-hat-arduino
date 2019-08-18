e6e9794e7329433e519bfa8e7e29ddf8bf88520e
Kyle5G8 <34083234+Kyle5G8@users.noreply.github.com>
2019-08-18 16:49:18 -0700

    Update variant.h

--- custom_board/RoboHat-0.0.12/variants/robohat/variant.h
+++ custom_board/RoboHat-0.0.12/variants/robohat/variant.h
@@ -1,23 +1,20 @@
 /*
   Copyright (c) 2014-2015 Arduino LLC.  All right reserved.
-
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
   version 2.1 of the License, or (at your option) any later version.
-
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   See the GNU Lesser General Public License for more details.
-
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#ifndef _VARIANT_ITSYBITSY_ZERO_
-#define _VARIANT_ITSYBITSY_ZERO_
+#ifndef _VARIANT_ARDUINO_ZERO_
+#define _VARIANT_ARDUINO_ZERO_
 
 // The definitions here needs a SAMD core >=1.6.10
 #define ARDUINO_SAMD_VARIANT_COMPLIANCE 10610
@@ -53,11 +50,14 @@ extern "C"
  *----------------------------------------------------------------------------*/
 
 // Number of pins defined in PinDescription array
-#define PINS_COUNT           (26u)
-#define NUM_DIGITAL_PINS     (20u)
-#define NUM_ANALOG_INPUTS    (12u)
-#define NUM_ANALOG_OUTPUTS   (1u)
-#define analogInputToDigitalPin(p)  ((p < 12u) ? (p) + PIN_A0 : -1)
+#ifdef __cplusplus
+extern "C" unsigned int PINCOUNT_fn();
+#endif
+#define PINS_COUNT           (PINCOUNT_fn())
+#define NUM_DIGITAL_PINS     (21u)
+#define NUM_ANALOG_INPUTS    (8u)
+#define NUM_ANALOG_OUTPUTS   (0u)
+#define analogInputToDigitalPin(p)  ((p < 8u) ? (p) + 14u : -1)
 
 #define digitalPinToPort(P)        ( &(PORT->Group[g_APinDescription[P].ulPort]) )
 #define digitalPinToBitMask(P)     ( 1 << g_APinDescription[P].ulPin )
@@ -75,62 +75,18 @@ extern "C"
  * https://github.com/arduino/Arduino/issues/1833
  */
 // #define digitalPinToTimer(P)
-
-// LEDs
-#define PIN_LED_13           (13u)
-#define PIN_LED              PIN_LED_13
-#define LED_BUILTIN          PIN_LED_13
-
-
-/* ROBO HAT MM1 pins */
-#define ROBOHAT_SERVO_1      (2ul)
-#define ROBOHAT_SERVO_2      (ROBOHAT_SERVO_1 + 1)
-#define ROBOHAT_SERVO_3      (ROBOHAT_SERVO_2 + 1)
-#define ROBOHAT_SERVO_4      (ROBOHAT_SERVO_3 + 1)
-#define ROBOHAT_SERVO_5      (ROBOHAT_SERVO_4 + 1)
-#define ROBOHAT_SERVO_6      (ROBOHAT_SERVO_5 + 1)
-#define ROBOHAT_SERVO_7      (ROBOHAT_SERVO_6 + 1)
-#define ROBOHAT_SERVO_8      (ROBOHAT_SERVO_7 + 1)
-
-#define ROBOHAT_EXTERNAL_NEOPIXEL (10ul)
-
-#define ROBOHAT_RCH_1      (PIN_A9)
-#define ROBOHAT_RCH_2      (ROBOHAT_RCH_1 + 1)
-#define ROBOHAT_RCH_3      (ROBOHAT_RCH_2 + 1)
-#define ROBOHAT_RCH_4      (ROBOHAT_RCH_3 + 1)
-
-#define ROBOHAT_SIGNAL_1     (15ul)
-#define ROBOHAT_SIGNAL_2     (16ul)
-#define ROBOHAT_SIGNAL_3     (11ul)
-#define ROBOHAT_SIGNAL_4     (12ul)
-#define ROBOHAT_SIGNAL_5     (17ul)
-#define ROBOHAT_SIGNAL_6     (18ul)
-#define ROBOHAT_SIGNAL_7     (35ul)  // TX: serial 
-#define ROBOHAT_SIGNAL_8     (36ul)  // RX: serial
-#define ROBOHAT_SIGNAL_9     (19ul)
-#define ROBOHAT_SIGNAL_10     (20ul)
-#define ROBOHAT_SIGNAL_11     (13ul)
-
-#define ROBOHAT_IRQ          (22ul)
-
 /*
  * Analog pins
  */
-#define PIN_A0               (14ul)
-#define PIN_A1               (PIN_A0 + 1)
-#define PIN_A2               (PIN_A0 + 2)
-#define PIN_A3               (PIN_A0 + 3)
-#define PIN_A4               (PIN_A0 + 4)
-#define PIN_A5               (PIN_A0 + 5)
-#define PIN_A6               (PIN_A0 + 6)
-#define PIN_A7               (PIN_A0 + 7)
-#define PIN_A8               (PIN_A0 + 8)
-#define PIN_A9               (PIN_A0 + 9)
-#define PIN_A10              (PIN_A0 + 10)
-#define PIN_A11              (PIN_A0 + 11)
-#define PIN_A12              (PIN_A0 + 12)
-#define PIN_A13              (PIN_A0 + 13)
-#define PIN_DAC0             (14ul)
+#define PIN_A0               (12ul)
+#define PIN_A1               (13ul)
+#define PIN_A2               (14ul)
+#define PIN_A3               (15ul)
+#define PIN_A4               (16ul)
+#define PIN_A5               (17ul)
+#define PIN_A6               (18ul)
+#define PIN_A7	             (19ul)
+
 
 static const uint8_t A0  = PIN_A0;
 static const uint8_t A1  = PIN_A1;
@@ -138,57 +94,64 @@ static const uint8_t A2  = PIN_A2;
 static const uint8_t A3  = PIN_A3;
 static const uint8_t A4  = PIN_A4;
 static const uint8_t A5  = PIN_A5;
-static const uint8_t A6  = PIN_A6 ;
-static const uint8_t A7  = PIN_A7 ;
-static const uint8_t A8  = PIN_A8 ;
-static const uint8_t A9  = PIN_A9 ;
-static const uint8_t A10 = PIN_A10 ;
-static const uint8_t A11 = PIN_A11 ;
-static const uint8_t A12 = PIN_A12 ;
-static const uint8_t A13 = PIN_A13 ;
-static const uint8_t DAC0 = PIN_DAC0;
+static const uint8_t A6  = PIN_A6;
+static const uint8_t A7  = PIN_A7;
+
+#define PIN_D1				(0ul)
+#define PIN_D2				(1ul)
+#define PIN_D3				(2ul)
+#define PIN_D4				(3ul)
+#define PIN_D5				(4ul)
+#define PIN_D6				(5ul)
+#define PIN_D7				(6ul)
+#define PIN_D8				(7ul)
+#define PIN_D9				(8ul)
+#define PIN_D10				(9ul)
+#define PIN_D11				(10ul)
+#define PIN_D12				(11ul)
+
+static const uint8_t D1  = PIN_D1;
+static const uint8_t D2  = PIN_D2;
+static const uint8_t D3  = PIN_D3;
+static const uint8_t D4  = PIN_D4;
+static const uint8_t D5  = PIN_D5;
+static const uint8_t D6  = PIN_D6;
+static const uint8_t D7  = PIN_D7;
+static const uint8_t D8  = PIN_D8;
+static const uint8_t D9  = PIN_D9;
+static const uint8_t D10  = PIN_D10;
+static const uint8_t D11  = PIN_D11;
+static const uint8_t D12  = PIN_D12;
 
 #define ADC_RESOLUTION		12
 
 // Other pins
-#define PIN_ATN              (38ul)
-static const uint8_t ATN = PIN_ATN;
+//#define PIN_ATN              (38ul)
+//static const uint8_t ATN = PIN_ATN;
 
 /*
  * Serial interfaces
  */
-
 // Serial1
-#define PIN_SERIAL1_TX       (35ul)
-#define PIN_SERIAL1_RX       (36ul)
+#define PIN_SERIAL_RX       (25ul)
+#define PIN_SERIAL_TX       (26ul)
+#define PAD_SERIAL_TX       (UART_TX_PAD_3)
+#define PAD_SERIAL_RX       (SERCOM_RX_PAD_2)
+
+// Serial2
+#define PIN_SERIAL1_RX       (27ul)
+#define PIN_SERIAL1_TX       (28ul)
 #define PAD_SERIAL1_TX       (UART_TX_PAD_2)
 #define PAD_SERIAL1_RX       (SERCOM_RX_PAD_3)
 
-/*
- * SPI Interfaces
- */
-#define SPI_INTERFACES_COUNT 1
-
-/* SPI FLASH */
-#define PIN_SPI_MISO         (28u)
-#define PIN_SPI_MOSI         (29u)
-#define PIN_SPI_SCK          (30u)
-#define PERIPH_SPI           sercom2
-#define PAD_SPI_TX           SPI_PAD_0_SCK_1
-#define PAD_SPI_RX           SERCOM_RX_PAD_2
-
-static const uint8_t SS   = 31 ;	// HW SS isn't used. Set here only for reference.
-static const uint8_t MOSI = PIN_SPI_MOSI ;
-static const uint8_t MISO = PIN_SPI_MISO ;
-static const uint8_t SCK  = PIN_SPI_SCK ;
 
 /*
  * Wire Interfaces
  */
 #define WIRE_INTERFACES_COUNT 1
 
-#define PIN_WIRE_SDA         (0u)
-#define PIN_WIRE_SCL         (1u)
+#define PIN_WIRE_SDA         (20u)
+#define PIN_WIRE_SCL         (21u)
 #define PERIPH_WIRE          sercom3
 #define WIRE_IT_HANDLER      SERCOM3_Handler
 
@@ -198,13 +161,10 @@ static const uint8_t SCL = PIN_WIRE_SCL;
 /*
  * USB
  */
-#define PIN_USB_HOST_ENABLE (32ul)
-#define PIN_USB_DM          (33ul)
-#define PIN_USB_DP          (34ul)
-/*
- * I2S Interfaces
- */
-#define I2S_INTERFACES_COUNT 0
+#define PIN_USB_HOST_ENABLE (22ul)
+#define PIN_USB_DM          (23ul)
+#define PIN_USB_DP          (24ul)
+
 
 #ifdef __cplusplus
 }
@@ -227,8 +187,18 @@ extern SERCOM sercom3;
 extern SERCOM sercom4;
 extern SERCOM sercom5;
 
+extern Uart Serial;
 extern Uart Serial1;
+extern Uart Serial2;
 
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+unsigned int PINCOUNT_fn();
+#ifdef __cplusplus
+}
 #endif
 
 // These serial port names are intended to allow libraries and architecture-neutral
@@ -246,11 +216,13 @@ extern Uart Serial1;
 //
 // SERIAL_PORT_HARDWARE_OPEN  Hardware serial ports which are open for use.  Their RX & TX
 //                            pins are NOT connected to anything by default.
-#define SERIAL_PORT_USBVIRTUAL      Serial
+#define SERIAL_PORT_USBVIRTUAL      SerialUSB
 #define SERIAL_PORT_MONITOR         Serial
 // Serial has no physical pins broken out, so it's not listed as HARDWARE port
 #define SERIAL_PORT_HARDWARE        Serial1
 #define SERIAL_PORT_HARDWARE_OPEN   Serial1
 
-#endif /* _VARIANT_ARDUINO_ZERO_ */
+#define SERIAL_PORT_HARDWARE        Serial2
+#define SERIAL_PORT_HARDWARE_OPEN   Serial2
 
+#endif /* _VARIANT_ARDUINO_ZERO_ */
